9ae65c7c4562eb1a00c025cba9ec638d7110c9c5
Thorgram <johnbaraniecki@hotmail.com>
2017-12-09 10:53:19 -0600

    hmm
    hmm

--- weapons/nanoboom3T.lua
+++ weapons/nanoboom3T.lua
@@ -1,32 +1,22 @@
--- WEAPONDEF -- NANOBOOM3T --
---------------------------------------------------------------------------------
-
-local weaponName = "nanoboom3T"
-
---------------------------------------------------------------------------------
-
-local weaponDef = {
-	areaofeffect = 160,
-	craterboost = 0,
-	cratermult = 0,
-	edgeeffectiveness = 0.75,
-	explosiongenerator = [[custom:newnanoboomtll]],
-	impulseboost = 0.12300000339746,
-	impulsefactor = 0.12300000339746,
-	name = [[MEX]],
-	range = 160,
-	reloadtime = 3.5999999046326,
-	soundhit = [[xplosml3]],
-	soundstart = [[largegun]],
-	turret = 1,
-	weaponvelocity = 70,
-	damage = {
-		default = 10,
-		nanos = 1050,
+return {
+	nanoboom3t = {
+		areaofeffect = 160,
+		craterboost = 0,
+		cratermult = 0,
+		edgeeffectiveness = 0.75,
+		explosiongenerator = "custom:newnanoboomtll",
+		impulseboost = 0.123,
+		impulsefactor = 0.123,
+		name = "MEX",
+		range = 160,
+		reloadtime = 3.6,
+		soundhit = "xplosml3",
+		soundstart = "largegun",
+		turret = 1,
+		weaponvelocity = 70,
+		damage = {
+			default = 10,
+			nanos = 1050,
+		},
 	},
 }
---------------------------------------------------------------------------------
-
-return lowerkeys({[weaponName] = weaponDef})
-
---------------------------------------------------------------------------------
